10f979aceb83f01045d4e87b49a8aa148e9e284c
Angel Newton <xanewton@gmail.com>
2017-10-28 20:44:25 -0400

    Add a count and label member variables

--- ClickCounter/ViewController.swift
+++ ClickCounter/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var count = 0
+    var label : UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,6 +23,7 @@ class ViewController: UIViewController {
         label.frame = CGRect.init(x: 150, y: 150, width: 60, height: 60)
         label.text = "0"
         self.view.addSubview(label)
+        self.label = label
         
         // Button
         var button = UIButton()
@@ -33,6 +38,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func incrementCount() {
+        self.count = self.count + 1
+        self.label.text = "\(self.count)"
+    }
 
 }
 
