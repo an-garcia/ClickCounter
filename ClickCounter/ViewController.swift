f9695071e8fa56001e59b3fa76369e68f55e9656
Angel Newton <xanewton@gmail.com>
2017-10-28 20:38:16 -0400

    Add a button

--- ClickCounter/ViewController.swift
+++ ClickCounter/ViewController.swift
@@ -14,11 +14,18 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        // Label
         var label = UILabel()
-        label.frame = CGRect.init(x:150, y:150, width:60, height:60)
+        label.frame = CGRect.init(x: 150, y: 150, width: 60, height: 60)
         label.text = "0"
-        
         self.view.addSubview(label)
+        
+        // Button
+        var button = UIButton()
+        button.frame = CGRect.init(x: 150, y: 250, width: 60, height: 60)
+        button.setTitle("Click", for: .normal)
+        button.setTitleColor( UIColor.blue, for: .normal)
+        self.view.addSubview(button)
     }
 
     override func didReceiveMemoryWarning() {
